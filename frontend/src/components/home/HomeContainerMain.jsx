21d7732773c28b3b274301e51f40da163693c076
Sparshlohana <sparsh.lohana@gmail.com>
2023-02-22 18:56:35 +0530

    Home page carousel

---
+++ frontend/src/components/home/HomeContainerMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeCarousel from "./homeCarousel/HomeCarousel";
+
+const HomeContainerMain = () => {
+  return (
+    <div className="homeContainerMain">
+      <HomeCarousel />
+    </div>
+  );
+};
+
+export default HomeContainerMain;
