fd8ebb07053490c0fcaaa7fc479978b28fb871e1
Sparshlohana <sparsh.lohana@gmail.com>
2023-03-07 12:13:43 +0530

    user/edit

---
+++ frontend/src/components/profile/profileBannerAndPhoto/profileBanner/ProfileBanner.jsx
@@ -0,0 +1,15 @@
+import "./profileBanner.css";
+
+const ProfileBanner = () => {
+  return (
+    <div className="profileBannerContainer">
+      <img
+        className="profileBanner"
+        src="https://cdn.pixabay.com/photo/2015/11/19/08/52/banner-1050629__340.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default ProfileBanner;
