f27fbbc339ad20d681b6ef3e3aff03c772b81b5b
deepprajapati0815 <deepprajapati0815@gmail.com>
2023-03-10 18:59:32 +0530

    deployed v1

--- frontend/src/context/contanst.js
+++ frontend/src/context/contanst.js
@@ -1,7 +1,8 @@
 import NftMarketplace from "../smartcontract/build/contracts/NFTMarketplace.json";
 
 export const NFTMarketplaceAddress =
-  "0x2C1806696dCDDd41Ff9225A100CBC5c92E988352";
+  "0x4AA2F2e2777ff763096291Fd984B50c2Ec461d35";
 
 export const NFTMarketplaceABI = NftMarketplace.abi;
 
+//first deployment on test net : 0x2C1806696dCDDd41Ff9225A100CBC5c92E988352
